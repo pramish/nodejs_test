1a7cee7f06210f9caa7ff8b51c43a2b35b0e8758
Pramish <pramish.luitel7@gmail.com>
2021-03-04 20:44:55 +1100

    Test one compelete

--- client/src/component/divisor.jsx
+++ client/src/component/divisor.jsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from "react";
 
 export const Divisor = () => {
-  const [divisorValue, setDivisorValue] = useState();
+  const [divisorValue, setDivisorValue] = useState([]);
   const [dataReady, setDataReady] = useState(false);
   const getDivisor = async () => {
     const response = await fetch("http://localhost:9999");
     const data = await response.json();
     if (data.success) {
-      setDivisorValue(data);
+      setDivisorValue(data.divisor);
       setDataReady(true);
       console.log(divisorValue);
-      console.log(data.success);
+    } else {
+      setDivisorValue([]);
+      setDataReady(false);
     }
   };
   return (
     <div>
       <h1>Get the Divisor</h1>
       <button onClick={getDivisor}>Get the Divisor Value</button>
-      {dataReady ? "Success" : "Fail"}
+      {dataReady
+        ? divisorValue.map((eac) => (
+            <p key={Object.keys(eac)}>
+              {Object.keys(eac)}:{Object.values(eac)}
+            </p>
+          ))
+        : "Failed to load data"}
     </div>
   );
 };
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,40 +7,47 @@ exports.getDivisor = async (request, response) => {
     const rangeData = await rangeRes.json();
     const divisorRes = await fetch(process.env.DIVISOR_ENDPOINT);
     const divisorData = await divisorRes.json();
-    divisorData.outputDetails.forEach((eachDivisor) => {
-      if (eachDivisor.divisor === 3) threeDivisor = eachDivisor.output;
-      if (eachDivisor.divisor === 5) fiveDivisor = eachDivisor.output;
-    });
-    if (
-      typeof rangeData.lower !== "number" ||
-      typeof rangeData.upper !== "number"
-    ) {
-      throw new Error("Couldn't get the Upper and Lower range of the number");
+    if (!divisorData.outputDetails) {
+      success = false;
+      divisor = [];
+    } else {
+      divisorData.outputDetails.forEach((eachDivisor) => {
+        if (eachDivisor.divisor === 3) threeDivisor = eachDivisor.output;
+        if (eachDivisor.divisor === 5) fiveDivisor = eachDivisor.output;
+      });
+      if (
+        typeof rangeData.lower !== "number" ||
+        typeof rangeData.upper !== "number"
+      ) {
+        success = false;
+        divisor = [];
+      } else {
+        for (var i = rangeData.lower + 1; i <= rangeData.upper; i++) {
+          if (i % 3 === 0) {
+            divisor.push({
+              [i]: fiveDivisor,
+            });
+          }
+          if (i % 5 === 0) {
+            divisor = [
+              ...divisor,
+              {
+                [i]: fiveDivisor,
+              },
+            ];
+          }
+          if (i % 3 === 0 && i % 5 === 0) {
+            divisor = [
+              ...divisor,
+              {
+                [i]: threeDivisor + fiveDivisor,
+              },
+            ];
+          }
+        }
+        success = true;
+      }
     }
-    for (var i = rangeData.lower + 1; i <= rangeData.upper; i++) {
-      if (i % 3 === 0) {
-        divisor.push({
-          [i]: fiveDivisor,
-        });
-      }
-      if (i % 5 === 0) {
-        divisor = [
-          ...divisor,
-          {
-            [i]: fiveDivisor,
-          },
-        ];
-      }
-      if (i % 3 === 0 && i % 5 === 0) {
-        divisor = [
-          ...divisor,
-          {
-            [i]: threeDivisor + fiveDivisor,
-          },
-        ];
-      }
-    }
-    success = true;
     return response.json({ divisor, success });
     // return divisor;
   } catch (error) {
